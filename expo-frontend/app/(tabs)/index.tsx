62c080ef2835f29cb8f9f62d67cf1a8361070034
Abby Castillo <56625600+abbymachines@users.noreply.github.com>
2026-01-20 10:49:07 -0800

    Web view + README (#3)
    - Make web view possible by conditionally loading caching logic only on mobile
    - Add README with instructions for how to run the app as a developer on Android and web

--- expo-frontend/app/(tabs)/index.tsx
+++ expo-frontend/app/(tabs)/index.tsx
@@ -14,33 +14,14 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/volery-logo-sketch.png')}
+          style={styles.voleryLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+            <ThemedText type="subtitle">Link to Explore modal</ThemedText>
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
@@ -66,21 +47,9 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">hello :3</ThemedText>
-        <ThemedText>{`i love to programming`}</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <AccountsList />
+        <ThemedText type="subtitle">Accounts List</ThemedText>
+        <ThemedText>{`(pulled from Salesforce)`}</ThemedText>
+        <ThemedText><AccountsList /></ThemedText>
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -96,7 +65,7 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
-  reactLogo: {
+  voleryLogo: {
     height: 178,
     width: 290,
     bottom: 0,
