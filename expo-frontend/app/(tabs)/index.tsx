7b4f17991e205317c2d0535bd0d2014761120917
Abby Castillo <abby.machines@gmail.com>
2026-02-17 14:35:40 -0800

    adjust header height, remove unnecessary text from home screen

--- expo-frontend/app/(tabs)/index.tsx
+++ expo-frontend/app/(tabs)/index.tsx
@@ -19,36 +19,6 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Link to Explore modal</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Sessions List</ThemedText>
-        <ThemedText>{`(pulled from Salesforce)`}</ThemedText>
         {/* <ThemedText><SessionsList /></ThemedText> */}
         <SessionsList />
       </ThemedView>
